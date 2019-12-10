1a27eb1479a9835ee0de58c7e35ad7962e512cd9
simon hylleberg <simonhylleberg@gmail.com>
2019-12-10 15:29:20 +0100

    derp

--- Bingo/MainWindow.xaml.cs
+++ Bingo/MainWindow.xaml.cs
@@ -22,26 +22,27 @@ namespace Bingo
     {
         List<int> BingoNumberList = new List<int>();
         List<int> BingoPladeNumberList = new List<int>();
+        List<int> BingoPladeMarkedList = new List<int>();
         public MainWindow()
         {
             InitializeComponent();
         }
         private void NumberGenerator_Click(object sender, RoutedEventArgs e)
         {
-            if(BingoNumberList.Count < 10)
+            if(BingoNumberList.Count < 100)
             {
-            Random ranGen = new Random();
-            bool AlreadyHasNumber = true;
-            int GeneratedNumber = 0;
-            do
-            {
-                GeneratedNumber = ranGen.Next(0, 100);
-                if(!BingoNumberList.Contains(GeneratedNumber))
+                Random ranGen = new Random();
+                bool AlreadyHasNumber = true;
+                int GeneratedNumber;
+                do
                 {
-                    AlreadyHasNumber = false;
+                    GeneratedNumber = ranGen.Next(0, 100);
+                    if(!BingoNumberList.Contains(GeneratedNumber))
+                    {
+                        AlreadyHasNumber = false;
+                    }
                 }
-            }
-            while (AlreadyHasNumber == true);
+                while (AlreadyHasNumber == true);
                 BingoNumberList.Add(GeneratedNumber);
                 NumberList.Items.Clear();
                 for (int i = 0; i < BingoNumberList.Count; i++)
@@ -58,6 +59,7 @@ namespace Bingo
 
         private void GenererPlade_Click(object sender, RoutedEventArgs e)
         {
+            BingoPladeNumberList.Clear();
             Random ranGen = new Random();
             List<Button> Btns = new List<Button>() {Btn1, Btn2, Btn3, Btn4, Btn5, Btn6, Btn7, Btn8, Btn9, Btn10,  Btn11, Btn12, Btn13, Btn14, Btn15, Btn16, Btn17, Btn18 };
             int incrementer = 0;
@@ -68,6 +70,96 @@ namespace Bingo
                 Btns[incrementer].Content = RanNumber;
                 incrementer++;
             }
+        }
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            BingoPladeMarkedList.Add(int.Parse(Btn1.Content.ToString()));
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn10_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn11_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn12_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn13_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn14_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn15_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn16_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn17_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn18_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
